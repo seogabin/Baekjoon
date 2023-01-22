7408f475c3cacd9b0f04ce6ed25e707d912ee8e1
Gabang <63779032+seogabin@users.noreply.github.com>
2023-01-22 16:29:02 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 16668 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <algorithm>
+#include <iostream>
+
+
+using namespace std;
+char arr[500000][30] = {0};
+
+int main() {
+	int num;
+	long long int a, b, c;
+	scanf("%lld %lld %lld", &a, &b, &c);
+	printf("%lld", a + b + c);
+	
+}
