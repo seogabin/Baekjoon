e0601a32167c920689c198e6be1d4db9bcb72d52
Gabang <63779032+seogabin@users.noreply.github.com>
2023-02-10 00:12:05 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <algorithm>
+#include <stack>
+#include <string.h>
+#include <iostream>
+
+#define MAX 100
+
+using namespace std;
+
+
+int main() {
+	
+	printf("         ,r\'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |");
+
+
+}
