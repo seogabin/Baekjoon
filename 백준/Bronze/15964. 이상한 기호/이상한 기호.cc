d4cd1a427a714825845f6477a1cc148a9985454c
Gabang <63779032+seogabin@users.noreply.github.com>
2023-02-13 23:42:05 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <algorithm>
+#include <stack>
+#include <string.h>
+#include <iostream>
+
+#define MAX 100
+
+using namespace std;
+
+int brr[10000]={0};
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d\n", (a + b) * (a - b));
+
+}
+
+	
+
