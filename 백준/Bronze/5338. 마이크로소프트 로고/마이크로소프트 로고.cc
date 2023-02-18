a075a28a9b40cb59cf35463cfd0639fecd3cd1df
Gabang <63779032+seogabin@users.noreply.github.com>
2023-02-19 00:23:53 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <algorithm>
+#include <stack>
+#include <string.h>
+#include <iostream>
+
+#define MAX 100
+
+using namespace std;
+
+int main() {
+	printf("       _.-;;-._\n'-..-'|   ||   |\n'-..-'|_.-;;-._|\n'-..-'|   ||   |\n'-..-'|_.-''-._|");
+
+}
+
+
+
+
