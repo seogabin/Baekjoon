ec8f1cb2332e1e654280ed48257d497011f684e6
Gabang <63779032+seogabin@users.noreply.github.com>
2023-02-18 23:24:38 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 0 ms, Memory: 21552 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.cc
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <algorithm>
+#include <stack>
+#include <string.h>
+#include <iostream>
+
+#define MAX 100
+
+using namespace std;
+
+char arr[10000000] = { 0 }, brr[10000000]={0};
+int main() {
+
+	int a, b;
+	printf("%d\n", 2023);
+	printf("%d\n", 02);
+	printf("%d\n", 18);
+
+}
+
+
+
+
